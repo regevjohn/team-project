83a9b4ea2fe7d7027ee741d304193719a592c042
Lior Sharon <62351450+lior1sharon@users.noreply.github.com>
2020-03-22 20:46:08 +0200

    Add files via upload

---
+++ video_client.py
@@ -0,0 +1,30 @@
+import socket
+import pickle
+import cv2
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+video = cv2.VideoCapture(0)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    while True:
+        ret, frame = video.read()
+        # print(sys.getsizeof(frame))
+        s.sendall(pickle.dumps(frame))
+
+        data = []
+        while True:
+            try:
+                s.settimeout(0.001)
+                packet = s.recv(4096)
+                if not packet:
+                    break
+                data.append(packet)
+            except socket.timeout:
+                break
+
+        if data:
+            cv2.imshow('image', pickle.loads(b''.join(data)))
+            cv2.waitKey(1)
---
+++ video_server.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = []
+            while True:
+                try:
+                    conn.settimeout(0.001)
+                    packet = conn.recv(4096)
+                    if not packet:
+                        break
+                    data.append(packet)
+                except socket.timeout:
+                    break
+            if not data:
+                continue
+            conn.sendall(b''.join(data))
